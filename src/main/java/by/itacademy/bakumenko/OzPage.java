e31967c2d6920cd13be0b9e9688a07bd6b537547
XeniaBkmnk <128148178+xeniabkmnk@users.noreply.github.com>
2023-11-17 20:02:39 +0100

    Update MangalibTest.java

--- src/main/java/by/itacademy/bakumenko/OzPage.java
+++ src/main/java/by/itacademy/bakumenko/OzPage.java
@@ -4,7 +4,8 @@ public class OzPage {
     public String errorMessageLocator = "//*[@id=\"test\"]/div/div";
     public String buttonSubmitLocator = "//*[@id=\"loginForm\"]/button";
     public String inputPasswordLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[2]/input";
+    public String enterTitleOauthFormLocator = "//*[@id=\"loginPopupIntro\"]";
     public String inputEmailLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[1]/input";
     public String buttonLoginOptionLocator = "//*[@id=\"loginFormLoginEmailLink\"]";
-    public String buttonEnterLocator = "//*[@id=\"top-page\"]/header/div[1]/div/div[1]/a";
+    public String buttonEnterLocatorBy = "//*[@id=\"top-page\"]/header/div[1]/div/div[1]/a";
 }
