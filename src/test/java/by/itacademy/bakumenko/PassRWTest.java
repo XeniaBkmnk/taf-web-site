135b91b5879be78a3f04eed4d6299ec98ffff166
XeniaBkmnk <128148178+xeniabkmnk@users.noreply.github.com>
2023-11-23 14:15:51 +0100

    Add testPassRwLoginEmptyError

--- src/test/java/by/itacademy/bakumenko/PassRWTest.java
+++ src/test/java/by/itacademy/bakumenko/PassRWTest.java
@@ -25,4 +25,35 @@ public class PassRWTest {
         Assertions.assertEquals("Авторизация", actual);
         driver.close();
     }
+<<<<<<< HEAD
+=======
+
+    @Test
+    public void testPassRwLoginEmptyError() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://pass.rw.by/ru/");
+        String enterButtonLocator = "//*[@id=\"db\"]/div[1]/div[1]/div/header/div/div[3]/div[3]/ul/li[2]/a";
+        By enterButtonBy = By.xpath(enterButtonLocator);
+        WebElement enterByttonWebElement = driver.findElement(enterButtonBy);
+        enterByttonWebElement.click();
+        String inputEnter = "//*[@id=\"form-auth\"]/fieldset/div[3]/input";
+        By inputEnterBy = By.xpath(inputEnter);
+        WebElement inputEnterWebElement = driver.findElement(inputEnterBy);
+        inputEnterWebElement.click();
+        String errorMessageLoginLocator = "//*[@id=\"login-error\"]";
+        By errorMessageLoginBy = By.xpath(errorMessageLoginLocator);
+        Thread.sleep(500);
+        WebElement errorMessageLoginWebElement = driver.findElement(errorMessageLoginBy);
+        String actuaiLogin = errorMessageLoginWebElement.getText();
+        Assertions.assertEquals("Заполните поле", actuaiLogin);
+        String errorMessageLoginPassword = "//*[@id=\"password-error\"]";
+        By errorMessagePasswordBy = By.xpath(errorMessageLoginPassword);
+        Thread.sleep(500);
+        WebElement errorMessagePasswordWebElement = driver.findElement(errorMessagePasswordBy);
+        String actualPassword = errorMessagePasswordWebElement.getText();
+        Assertions.assertEquals("Заполните поле", actualPassword);
+        driver.close();
+    }
+>>>>>>> 1d4a81d (Add method testPassRwLoginEmptyError)
 }
