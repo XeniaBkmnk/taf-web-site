318899d329fb2a09582c0bf3a94884534d1f2e15
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2023-11-25 10:07:37 +0300

    Updete

--- src/main/java/by/itacademy/bakumenko/OzPage.java
+++ src/main/java/by/itacademy/bakumenko/OzPage.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class OzPage {
-    public String errorMessageLocator = "//*[@id=\"test\"]/div/div";
-    public String buttonSubmitLocator = "//*[@id=\"loginForm\"]/button";
-    public String inputPasswordLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[2]/input";
-    public String enterTitleOauthFormLocator = "//*[@id=\"loginPopupIntro\"]";
-    public String inputEmailLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[1]/input";
-    public String buttonLoginOptionLocator = "//*[@id=\"loginFormLoginEmailLink\"]";
-    public String buttonEnterLocatorBy = "//*[@id=\"top-page\"]/header/div[1]/div/div[1]/a";
+    private String errorMessageLocator = "//*[@id=\"test\"]/div/div";
+    private String buttonSubmitLocator = "//*[@id=\"loginForm\"]/button";
+    private String inputPasswordLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[2]/input";
+    private String enterTitleOauthFormLocator = "//*[@id=\"loginPopupIntro\"]";
+    private String inputEmailLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[1]/input";
+    private String buttonLoginOptionLocator = "//*[@id=\"loginFormLoginEmailLink\"]";
+    private String buttonEnterLocatorBy = "//*[@id=\"top-page\"]/header/div[1]/div/div[1]/a";
 
     WebDriver driver;
 
