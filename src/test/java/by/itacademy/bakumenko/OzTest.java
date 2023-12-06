2a734a9f5d1f1aa21e0ff624492984ad8ad1ae55
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2023-12-06 18:46:41 +0300

    update class OzTest

--- src/test/java/by/itacademy/bakumenko/OzTest.java
+++ src/test/java/by/itacademy/bakumenko/OzTest.java
@@ -1,19 +1,20 @@
 package by.itacademy.bakumenko;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OzTest {
+public class OzTest extends BaseTest {
+    OzPage ozPage;
+
+    @BeforeEach
+    public void start() {
+        driver.get("https://oz.by/");
+        ozPage = new OzPage(driver);
+    }
+
     @Test
     public void testOzLogin() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://oz.by/");
-        OzPage ozPage = new OzPage(driver);
         ozPage.clickButtonEnterLoginLocatorBy();
         ozPage.clickButtonLoginOptionLocator();
         ozPage.sendKeysInputEmailLocator("test@test");
@@ -22,15 +23,10 @@ public class OzTest {
         ozPage.getErrorMessageLocator();
         String actual = ozPage.getErrorMessageLocator();
         Assertions.assertEquals("Введите корректный адрес электронной почты", actual);
-        driver.close();
     }
 
     @Test
     public void testOzPasaword() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://oz.by/");
-        OzPage ozPage = new OzPage(driver);
         ozPage.clickButtonEnterLoginLocatorBy();
         ozPage.clickButtonLoginOptionLocator();
         ozPage.sendKeysInputEmailLocator("test@gmail.com");
@@ -38,18 +34,13 @@ public class OzTest {
         ozPage.clickButtonSubmitLocator();
         String actual = ozPage.getErrorMessageLocator();
         Assertions.assertEquals("Неверный пароль. Если вы потеряли или забыли пароль — восстановите его", actual);
-        driver.close();
     }
 
     @Test
     public void testOzEntry() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://oz.by/");
-        OzPage ozPage = new OzPage(driver);
         ozPage.clickButtonEnterLoginLocatorBy();
         String actual = ozPage.getEnterTitleOauthFormLocator();
         Assertions.assertEquals("Вход", actual);
-        driver.close();
     }
+
 }
